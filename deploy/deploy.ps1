ddc84a6b012c05d31f6007a9da7ce376925339a5
Brian Evans <ebrian101@gmail.com>
2025-10-03 19:19:44 +0100

    Add workspace file

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -8,7 +8,8 @@ ssh flower "chmod -R 755 ~/companies-catalogue/bin"
 
 scp pipeline.py root@flower:~/companies-catalogue/
 scp requirements.txt root@flower:~/companies-catalogue/
+scp workspace.yaml root@flower:~/companies-catalogue/
 
 # setup python environment with deps. run dagster
-ssh flower "cd companies-catalogue && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt && dagster job execute -f pipeline.py -j companies_catalogue_job"
-# dagster-daemon run --python-file pipeline.py
+ssh flower "cd companies-catalogue && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt && nohup dagster-daemon run &"
+# dagster job execute -f pipeline.py -j companies_catalogue_job
