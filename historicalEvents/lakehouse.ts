d064943fe9d8e8e7056809f9129c2832ce1ec5c6
Brian Evans <ebrian101@gmail.com>
2026-01-31 12:06:21 +0000

    Event ingestion to lakehouse
    Add logic to handle specific streams, validate against a predefined list, and load files into the lakehouse. Create metadata tracking for loaded files, temporary table usage for batch processing, and enhance schema setup. Main function updated to accept streamPath as an argument.

--- historicalEvents/lakehouse.ts
+++ historicalEvents/lakehouse.ts
@@ -1,21 +1,19 @@
-
 // This is to move events from the lake (.json.gz) to a Ducklake (parquet lakehouse with metadata)
 // Ducklake catalog is frozen on S3.
 
 import {DuckDBConnection, DuckDBInstance} from "@duckdb/node-api";
 import {randomUUIDv7, S3Client} from "bun";
-import {createReadStream, createWriteStream} from 'node:fs'
 import {tmpdir} from 'node:os'
-import {Readable} from "node:stream";
-import {pipeline} from "node:stream/promises";
+import {streams} from "./utils.js";
 
-async function setupLakehouseConnection(){
+async function setupLakehouseConnection() {
 
-const tmpDbFilepath = tmpdir() + `/${randomUUIDv7()}_catalogue.ducklake`
-const tempDbFile = Bun.file(tmpDbFilepath)
-const db = await DuckDBInstance.create(':memory:');
-const connection = await db.connect();
-await connection.run(`
+    const tmpDbFilepath = tmpdir() + `/${randomUUIDv7()}_catalogue.ducklake`
+    const tmpDuckdbFilepath = tmpdir() + `/${randomUUIDv7()}_catalogue.duckdb`
+    const tempDbFile = Bun.file(tmpDbFilepath)
+    const db = await DuckDBInstance.create(':memory:');
+    const connection = await db.connect();
+    await connection.run(`
 INSTALL httpfs;
 LOAD httpfs;
 INSTALL ducklake;
@@ -43,23 +41,28 @@ CREATE SECRET lakehouse (
     METADATA_PATH '${tempDbFile.name}',
     DATA_PATH 's3://companies-stream-lake/'
 );
+ATTACH '${tmpDuckdbFilepath}' AS local_db;
 `)
 
-    const remoteCataloguePath='catalogue.ducklake'
+    const remoteCataloguePath = 'catalogue.ducklake'
     const catalogueDbFile = lakeBucket.file(remoteCataloguePath)
-    if(await catalogueDbFile.exists()) {
+    if (await catalogueDbFile.exists()) {
         await tempDbFile.write(await catalogueDbFile.bytes())
         console.log('downloaded lakehouse catalogue to', tempDbFile.name)
-    }else{
+    } else {
         console.log('no lakehouse catalogue found, creating one.')
     }
 
     await connection.run(`ATTACH 'ducklake:lakehouse' AS lakehouse (CREATE_IF_NOT_EXISTS true);`)
     await connection.run(`USE lakehouse;`)
-    return {connection, tempDbFile,remoteCataloguePath}
+    return {connection, tempDbFile, remoteCataloguePath}
 }
 
-async function saveAndCloseLakehouse({connection, tempDbFile,remoteCataloguePath}:{connection: DuckDBConnection, tempDbFile: Bun.BunFile, remoteCataloguePath: string}){
+async function saveAndCloseLakehouse({connection, tempDbFile, remoteCataloguePath}: {
+    connection: DuckDBConnection,
+    tempDbFile: Bun.BunFile,
+    remoteCataloguePath: string
+}) {
     await connection.run(`
     ATTACH ':memory:' AS memory_db;
     USE memory_db;
@@ -71,25 +74,75 @@ async function saveAndCloseLakehouse({connection, tempDbFile,remoteCataloguePath
 
 const lakeBucket = new S3Client({bucket: 'companies-stream-lake'})
 
-async function main() {
-
-    const {connection, tempDbFile,remoteCataloguePath} = await setupLakehouseConnection()
+async function main(streamPath: string) {
+    if (!streams.includes(streamPath)) {
+        console.log('stream', streamPath, 'not in streams list, skipping')
+        return
+    }
+    console.log('Loading', streamPath, 'into lakehouse')
+    const {connection, tempDbFile, remoteCataloguePath} = await setupLakehouseConnection()
 
     const tablesRes = await connection.runAndReadAll(`SHOW TABLES;`)
-    const tables = tablesRes.getRowObjects().map(r=>r.name as string)
+    const tables = tablesRes.getRowObjects().map(r => r.name as string)
     console.log('tables in lakehouse', tables)
 
     await connection.run(`
-    CREATE TABLE IF NOT EXISTS events (
-        resource_kind VARCHAR,
-        resource_id VARCHAR,
-        resource_uri VARCHAR,
-        "data" JSON,
-        "event" STRUCT(timepoint BIGINT, published_at VARCHAR, "type" VARCHAR)
-    )
+        CREATE TABLE IF NOT EXISTS events
+        (
+            stream VARCHAR,
+            resource_kind VARCHAR,
+            resource_id VARCHAR,
+            resource_uri VARCHAR,
+            "data" JSON,
+            "event" STRUCT(timepoint BIGINT, published_at VARCHAR, "type" VARCHAR)
+        );
+        CREATE TABLE IF NOT EXISTS snapshot AS FROM events WITH NO DATA;
     `)
 
-    await saveAndCloseLakehouse({connection, tempDbFile,remoteCataloguePath})
+    await connection.run(`CREATE SCHEMA IF NOT EXISTS cc_metadata;`)
+    await connection.run(`CREATE TABLE IF NOT EXISTS cc_metadata.loaded_files
+                          (
+                              file VARCHAR
+                          )`)
+    // find files in S3 that aren't loaded
+    console.log('Finding files to load')
+    const res = await connection.runAndReadAll(`
+        SELECT file
+        FROM glob('s3://companies-stream-sink/${streamPath}/*.json.gz')
+        WHERE file NOT IN (SELECT file FROM cc_metadata.loaded_files)
+        ORDER BY file ASC
+        LIMIT 2;
+    `)
+
+    const files = res.getRowObjects().map(f => f.file as string)
+    if (files.length) {
+        console.log('Loading', files.length, 'files into lakehouse', files)
+
+
+        await connection.run(`CREATE OR REPLACE TABLE local_db.new_events_tmp AS FROM events WITH NO DATA;`)
+        await connection.run(`INSERT INTO local_db.new_events_tmp BY NAME
+                              SELECT '${streamPath}' as stream, *
+                              FROM read_json(${JSON.stringify(files)});`)
+        console.log('Loaded', files.length, 'files into new_events_tmp')
+        await connection.run('BEGIN TRANSACTION;')
+        await connection.runAndReadAll(`
+            INSERT INTO events BY NAME
+            (FROM local_db.new_events_tmp
+             WHERE event.timepoint > (SELECT MAX(event.timepoint) FROM events WHERE stream = '${streamPath}')
+                );`)
+        console.log('Loaded', files.length, 'files into main events table')
+
+
+        await connection.run(`INSERT INTO cc_metadata.loaded_files
+                              VALUES ${files.map(f => `('${f}')`).join(',')};`)
+        console.log('Updated loaded_files table with', files.length, 'new files')
+
+        await connection.run('COMMIT;')
+
+        await connection.run('DROP TABLE local_db.new_events_tmp;')
+        // await connection.run('CHECKPOINT;') // Do this once a week/month
+    }
+    await saveAndCloseLakehouse({connection, tempDbFile, remoteCataloguePath})
 }
 
-await main()
+await main(process.argv[2])
