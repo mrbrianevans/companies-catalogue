2007daf68a837fb705581ab75f5bbb8c5304e2a4
Brian Evans <ebrian101@gmail.com>
2026-03-07 14:12:29 +0000

    Load multiple files into lakehouse if needed
    Loops until there aren't any more to load.
    Loads them one by one.

--- lakehouse/lakehouse.ts
+++ lakehouse/lakehouse.ts
@@ -3,8 +3,11 @@
 
 import { executeSql, streams } from "./utils.js";
 import { saveAndCloseLakehouse, setupLakehouseConnection } from "./connection.js";
+
 import lakehouseSnapshotSql from "./lakehouse_snapshot.sql" with { type: "text" };
 import lakehouseEventsSql from "./lakehouse_events.sql" with { type: "text" };
+import lakehouseSetupSql from "./lakehouse_setup.sql" with { type: "text" };
+import { DuckDBListValue } from "@duckdb/node-api";
 
 const getSchema = (streamPath: string) => streamPath.replaceAll(/[^a-z0-9_]/gi, "_");
 
@@ -20,9 +23,21 @@ async function main(streamPath: string) {
   await connection.run(`SET VARIABLE SINK_BUCKET = '${process.env.SINK_BUCKET}';`);
   await connection.run(`SET VARIABLE streamPath = '${streamPath}';`);
 
-  console.time("load events");
-  await executeSql(connection, lakehouseEventsSql);
-  console.timeEnd("load events");
+  console.time("setup lakehouse");
+  await executeSql(connection, lakehouseSetupSql);
+  console.timeEnd("setup lakehouse");
+
+  while (true) {
+    const filesRemaining = await connection.runAndReadAll(`SELECT list(file) as files FROM
+    (FROM glob('s3://'||getvariable('SINK_BUCKET')||'/'||getvariable('streamPath')||'/*.json.gz')
+    WHERE file NOT IN (SELECT file FROM cc_metadata.loaded_files))`);
+    const files = filesRemaining.getRowObjects()[0].files as DuckDBListValue;
+    if (!files?.items?.length) break;
+    console.log("Files remaining", files.items);
+    console.time("load events");
+    await executeSql(connection, lakehouseEventsSql);
+    console.timeEnd("load events");
+  }
 
   console.log("Merging any unmerged events into the snapshot");
   console.time("merge snapshot");
