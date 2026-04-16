3e4bd25ae559619ec532ed72f13b705dfc30b50c
Brian Evans <ebrian101@gmail.com>
2026-04-16 17:11:04 +0100

    Add CORS rule and new private bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,15 @@
 resource "aws_s3_bucket" "stream_snapshots" {
   bucket        = "${var.bucket_prefix}companies-stream-snapshots"
   force_destroy = false
+  cors_rule {
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["http://localhost:5173", "https://companies-catalogue.co.uk"]
+  }
+}
+
+resource "aws_s3_bucket" "private_snapshots" {
+  bucket        = "${var.bucket_prefix}companies-stream-snapshots-private"
+  force_destroy = false
 }
 
 resource "aws_s3_bucket" "stream_sink" {
