9f7254a2fcb790918443051f0f40a6dc7deb290b
Brian Evans <ebrian101@gmail.com>
2026-04-16 17:51:04 +0100

    Delete snapshots after 3 days
    Since they have the date in them, new ones don't override old ones.
    They should be re-produced every 24 hours and signed links will expire after 24 hours.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,18 @@ resource "aws_s3_bucket" "stream_snapshots" {
 resource "aws_s3_bucket" "private_snapshots" {
   bucket        = "${var.bucket_prefix}companies-stream-snapshots-private"
   force_destroy = false
+  lifecycle_rule {
+    id                                     = "Default Multipart Abort Rule"
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 1
+  }
+  lifecycle_rule {
+    id      = "Delete after 3 days"
+    enabled = true
+    expiration {
+      days = 3
+    }
+  }
 }
 
 resource "aws_s3_bucket" "stream_sink" {
