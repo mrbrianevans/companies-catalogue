449665c2155156514b28cccff590b000ce946205
BrianEvans <ebrian101@gmail.com>
2026-05-04 15:14:09 +0100

    Reformat

--- lakehouse/lakehouse.ts
+++ lakehouse/lakehouse.ts
@@ -28,12 +28,12 @@ async function main(streamPath: string) {
   console.timeEnd("setup lakehouse");
 
   while (true) {
-    console.time('check for unloaded files')
+    console.time("check for unloaded files");
     const filesRemaining = await connection.runAndReadAll(`SELECT list(file) as files FROM
     (FROM glob('s3://'||getvariable('SINK_BUCKET')||'/'||getvariable('streamPath')||'/*.json.gz')
     WHERE file NOT IN (SELECT file FROM catalogue.cc_metadata.loaded_files))`);
     const files = filesRemaining.getRowObjects()[0].files as DuckDBListValue;
-    console.timeEnd('check for unloaded files')
+    console.timeEnd("check for unloaded files");
     console.log("Files remaining", files?.items?.length);
     if (!files?.items?.length) break;
     console.time("load events");
