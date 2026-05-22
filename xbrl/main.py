0963e71f0d42d8a30c8b3c0f1fa7553611b62423
BrianEvans <ebrian101@gmail.com>
2026-05-22 19:35:27 +0100

    Add initial python script
    Calls the sync function.
    Getting an error on listing existing files,
    might need a new bucket.

---
+++ xbrl/main.py
@@ -0,0 +1,11 @@
+import boto3
+from stream_read_xbrl import stream_read_xbrl_sync_s3_csv
+import os
+
+
+if __name__ == '__main__':
+    s3_client = boto3.client('s3', region_name=os.getenv("S3_REGION"), endpoint_url=os.getenv("S3_ENDPOINT"))
+    bucket_name = 'dev-companies-stream-sink'
+    key_prefix = 'xbrl/'
+
+    stream_read_xbrl_sync_s3_csv(s3_client, bucket_name, key_prefix)
