5d42022d1f5c31309112c66226505ef6f92ef35d
BrianEvans <ebrian101@gmail.com>
2026-06-06 12:34:37 +0100

    Add sftp schema setup

--- postgres_catalogue/schema_setup.sql
+++ postgres_catalogue/schema_setup.sql
@@ -11,3 +11,11 @@ create schema if not exists cc_metadata;
 grant all on schema cc_metadata to "read-write";
 
 grant select on ALL TABLES IN schema cc_metadata to "read-only";
+
+
+
+create schema if not exists sftp;
+
+grant all on schema sftp to "read-write";
+
+grant select on ALL TABLES IN schema sftp to "read-only";
