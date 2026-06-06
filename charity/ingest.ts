67dc5ee1f3675dd6808a8a6634960e2c51a72895
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2026-06-06 12:44:30 +0100

    Ingest Charity Commission (#17)
    * Add sftp schema setup
    
    * Add charity commission readme
    
    * Add ingest script draft
    
    * Reformat
    
    * Add daily job to create charity tables in lakehouse

---
+++ charity/ingest.ts
@@ -0,0 +1,31 @@
+import { setupLakehouseConnection } from "../lakehouse/connection.ts";
+import { executeSql } from "../lakehouse/utils.ts";
+
+async function loadCharityEntities() {
+  const { connection } = await setupLakehouseConnection();
+  await connection.run(`
+use lakehouse;
+create schema if not exists charity;
+use lakehouse.charity;
+INSTALL httpfs;
+LOAD httpfs;
+INSTALL zipfs FROM community;
+LOAD zipfs;
+`);
+  const entities = ["charity_other_regulators", "charity"];
+  for (const entity of entities) {
+    await executeSql(
+      connection,
+      `
+        CREATE OR REPLACE TABLE ${entity} AS (
+            SELECT * FROM read_json_objects(
+                'zip://https://ccewuksprdoneregsadata1.blob.core.windows.net/data/json/publicextract.${entity}.zip/publicextract.${entity}.json',
+                format = 'array'
+            )
+        );
+`,
+    );
+  }
+}
+
+await loadCharityEntities();
